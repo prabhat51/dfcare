638892c58e7cb25e995fb9c8a88924a8fc910cfd
prabhat51 <88582253+prabhat51@users.noreply.github.com>
2025-07-15 11:26:49 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DiabeticFootPredictor from './components/DiabeticFootPredictor';
+
+function App() {
+  return (
+    <div className="App">
+      <DiabeticFootPredictor />
+    </div>
+  );
+}
+
+export default App;
